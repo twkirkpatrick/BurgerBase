cbaf25ece773ed0f4d15e07c2917350844a81a26
Tanner Kirkpatrick <twk4491@gmail.com>
2020-11-11 14:17:03 -0500

    initial file structure created

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+const connection = require("./connection")
+
+//Methods//
+
+//selectAll()
+
+//insertOne()
+
+//updateOne()
+
+module.exports = orm;
+
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const burger = require("../models/burger");
+
+
+//create the router for the app, and export the router at end of file
+
